887e174fce15e6eae83caf1ac5d961d049e54b47
Master git <foobar@toto.fr>
2021-03-05 11:21:27 +0100

    Add URL validation with REGEX in get-urls.sh

--- get-urls.sh
+++ get-urls.sh
@@ -12,4 +12,13 @@ if [ $# -gt 1 ]; then
     exit 2
 fi
 
-wget -q -O - $1
+URL_REGEX='((https?:)?\/\/)?((([a-z0-9\-_]+\.)+[a-z]{2,})|(([0-9]{1,3}\.){3}[0-9]{1,3}))(\/[a-z0-9\-_]+)*\/?'
+
+COUNT=`echo "$1" | grep -i -E -o "$URL_REGEX" | wc -l`
+
+if [ $COUNT -eq 0 ]; then
+    echo "Not a valid URL."
+    exit 3
+fi
+
+#wget -q -O - $1
